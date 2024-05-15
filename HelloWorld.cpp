3db5879f8dcc569044490c98f44bde7962a1806c
Sarthak Srivastava <86970961+SarthakSrivastava257@users.noreply.github.com>
2024-05-15 18:57:39 +0000

    Number addition

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,9 @@ int main(){
     std::cout<<"Hello World"<<std::endl;
     std::cout<<"Sarthak Srivastava's Codespace Repository"<<std::endl;
     std::cout<<"This is new line"<<std::endl;
+    int num1=2, num2=5;
+    int sum=num1 + num2;
+    std::cout<<"The sum of Numbers is: "<<sum<<std::endl;
+    
     return 0;
 }

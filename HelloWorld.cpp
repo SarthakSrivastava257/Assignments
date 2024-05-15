f824c6681114b59bce1343ef913770b30c7e5eef
Sarthak Srivastava <86970961+SarthakSrivastava257@users.noreply.github.com>
2024-05-15 19:05:51 +0000

    This is Subtraction branch

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,6 +7,9 @@ int main(){
     int num1=2, num2=5;
     int sum=num1 + num2;
     std::cout<<"The sum of Numbers is: "<<sum<<std::endl;
-    
+
+    int sub=num2-num1;
+    std::cout<<"The substraction of Numbers is: "<<sub<<std::endl;
+
     return 0;
 }
